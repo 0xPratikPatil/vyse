7d094ba3b3ad8e5053652ac07ffe0ac418849564
0xPratikPatil <dev.pratikpatil@proton.me>
2025-06-02 16:08:44 +0530

    Fix:Demo Editor Link Added

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -135,8 +135,8 @@ export default function HeroSection() {
                     variant="ghost"
                     className="h-10.5 rounded-xl px-5"
                   >
-                    <Link href="#features">
-                      <span className="text-nowrap">See Features</span>
+                    <Link href="/demo">
+                      <span className="text-nowrap">See Demo</span>
                     </Link>
                   </Button>
                 </AnimatedGroup>
